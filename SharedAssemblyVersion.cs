0aef1287bed883c3b3e3e65b881ecb8eb002f44b
kinetic <kinetic@>
2016-04-23 19:50:53 +0300

    qq

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-using System;
 using System.Reflection;
 
 [assembly: AssemblyVersion("0.18.2")]
